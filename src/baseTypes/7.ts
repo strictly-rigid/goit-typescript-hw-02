c986519baf7227dddf66245376c5a3ac5d9d12a4
strictly-rigid <holysociology@gmail.com>
2025-02-09 09:50:21 +0100

    base types done + some generics

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,19 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekdays {
+  Monday = "Monday",
+  Tuesday = "Tuesday",
+  Wednesday = "Wednesday",
+  Thursday = "Thursday",
+  Friday = "Friday",
+  Saturday = "Saturday",
+  Sunday = "Sunday",
+}
+
+function isWeekend(day: Weekdays): boolean {
+  return day === Weekdays.Saturday || day === Weekdays.Sunday;
+}
+
+console.log(Weekdays.Monday);
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,19 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
-function compare (top, bottom): AllType {
+function compare<
+  T extends Pick<AllType, keyof AllType>,
+  K extends Pick<AllType, keyof AllType>
+>(top: T, bottom: K): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
